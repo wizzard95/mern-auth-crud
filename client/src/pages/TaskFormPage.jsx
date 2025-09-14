c9168b6d09ab1ba9530920a1b2a67f4edb2b9197
wizzard95 <damian110995a@gmail.com>
2025-09-14 20:57:44 -0300

    aplicando estilos al formulario de tareas

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -10,14 +10,16 @@ function TaskFormPage() {
   })
 
   return (
-    <div>
+    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
     <form onSubmit={onSubmit}>
       <input type="text" placeholder="Title" 
         {...register('title')}
+        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
         autoFocus
       />
       <textarea rows="3" placeholder="Decription"
         {...register('description')}
+        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
       ></textarea>
       <button>
         Save
