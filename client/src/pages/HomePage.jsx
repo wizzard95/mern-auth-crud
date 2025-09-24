56c16230c512d43d069989510e953116a4feff24
wizzard95 <damian110995a@gmail.com>
2025-09-24 16:18:50 -0300

    redireccion a la pagina de tareas desde el home.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
 
 
+ 
 function HomePage(){
   return (
-    <div>HomePage</div>
+   
+    <div>
+      <h1>home</h1>
+      <Link 
+          to="/tasks" 
+          style={{
+            color: '#3b82f6', 
+            textDecoration: 'none',
+            fontSize: '14px'
+          }}
+        >
+          ← Volver a Tareas
+        </Link>
+    </div>
+
+    
+    
   )
 }
 
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -19,7 +19,7 @@ function TasksPage() {
 
   return (
   
-        <div>
+        <div className="grid grid-cols-3 gap-2">
           {tasks.map((task) => (
             <TaskCard task={task} key={task._id}/>
           ))}
